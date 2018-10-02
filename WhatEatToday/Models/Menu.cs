f8a41938db9c2abe8fe3500337e33a9ed953d7ae
ePhat <ePhat@DESKTOP-GG1P0GF>
2018-10-02 16:51:35 +0700

    Add project files.

---
+++ WhatEatToday/Models/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhatEatToday.Models
+{
+    public class Menu
+    {
+        [Key]
+        public int menu_id { get; set; }
+
+        public int shop_id { get; set; }
+        public string menu_name { get; set; }
+        public string comment { get; set; }
+    }
+}
