4f23c8d7711d50f71d73f5760901e178efce29bf
Phattarintorn Srinang <Phatza@windowslive.com>
2018-10-06 14:49:49 +0700

    Add project files.

---
+++ WhatEatToday/Models/Shop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhatEatToday.Models
+{
+    public class Shop
+    {
+        [Key] //primary key
+        public int shop_id { get; set; }
+
+        public string name { get; set; }
+        public string location { get; set; }
+        public string details { get; set; }
+        public string type { get; set; }
+        public string pic { get; set; }
+
+    }
+}
