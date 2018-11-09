2493e601d058b43480492e28a3b99127bb558df9
MilkiizWhite <32997425+MilkiizWhite@users.noreply.github.com>
2018-11-09 12:59:52 +0700

    report view

--- WhatEatToday/Controllers/HomeController.cs
+++ WhatEatToday/Controllers/HomeController.cs
@@ -43,5 +43,6 @@ namespace WhatEatToday.Controllers
 
             return View();
         }
+
     }
 }
---
+++ WhatEatToday/Controllers/ReportsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WhatEatToday.Models;
+
+
+namespace WhatEatToday.Views.Reports
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult index()
+        {
+            return View();
+        }
+    }
+}
